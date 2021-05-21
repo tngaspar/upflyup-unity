ae1186ba0cb0c993175271ea1ee6a51f8e45ac1b
Tiago Gaspar <tiagongaspar@gmail.com>
2021-05-21 15:18:19 +0000

    1st c

---
+++ Assets/CamSmoothFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CamSmoothFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    private void FixedUpdate()
+    {
+        Vector3 desiredPosition = transform.position = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition; 
+    }
+}
---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerControl : MonoBehaviour
+{
+    public float power = 10f;
+    public float maxDrag = 5f;
+    public Rigidbody2D rb;
+    public LineRenderer lr;
+
+    Vector3 dragStartPos;
+    Touch touch;
+
+    float speed;
+    public float speedtoMove = .1f;
+
+    private void Update()
+    {
+        speed = rb.velocity.magnitude;
+        if (Input.touchCount > 0 && speed < speedtoMove)
+        {
+            touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                DragStart();
+            }
+            if (touch.phase == TouchPhase.Moved && lr.positionCount != 0)
+            {
+                Dragging();
+            }
+            if (touch.phase == TouchPhase.Ended && lr.positionCount != 0)
+            {
+                DragRelease();
+            }
+        }
+    }
+
+    void DragStart() {
+        dragStartPos = Camera.main.ScreenToWorldPoint(touch.position);
+        dragStartPos.z = 0f;
+        lr.positionCount = 1;
+        lr.SetPosition(0, dragStartPos);
+    }
+    void Dragging() {
+        Vector3 draggingPos = Camera.main.ScreenToWorldPoint(touch.position);
+        draggingPos.z = 0f;
+        lr.positionCount = 2;
+        lr.SetPosition(1, draggingPos);
+    }
+    void DragRelease() {
+        lr.positionCount = 0;
+
+        Vector3 dragReleasePos = Camera.main.ScreenToWorldPoint(touch.position);
+        dragReleasePos.z = 0f;
+        Vector3 force = dragStartPos - dragReleasePos;
+        Vector3 clampedForce = Vector3.ClampMagnitude(force, maxDrag) * power;
+
+        rb.AddForce(clampedForce, ForceMode2D.Impulse);
+
+    }
+
+
+
+}
