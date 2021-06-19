4fcca2aebe9ea49a8f6df26be166ad2e54e1d8a5
Tiago Gaspar <tiagongaspar@gmail.com>
2021-06-19 16:29:17 +0000

    implemented energy on player color

---
+++ Assets/PlayerEnergyTracker.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerEnergyTracker : MonoBehaviour
+{
+    public Color zeroMovesMade;
+    public Color oneMoveMade;
+    public Color twoMovesMade;
+
+    private void Update()
+    {
+        int movesMade = GetComponent<PlayerControl>().moves;
+        if (movesMade == 0)
+        {
+            GetComponent<SpriteRenderer>().color = zeroMovesMade;
+        }
+        else if (movesMade == 1)
+        {
+            GetComponent<SpriteRenderer>().color = oneMoveMade;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().color = twoMovesMade;
+        }
+    }
+}
