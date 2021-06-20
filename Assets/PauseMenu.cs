3e528ee1235d4544deeb376cb4daa42d57080af6
Tiago Gaspar <tiagongaspar@gmail.com>
2021-06-20 13:25:37 +0000

    pause menu implemented

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+    public void PauseButtonClick()
+    {
+        if (!GameIsPaused)
+        {
+            Pause();
+        }
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting application");
+        Application.Quit();
+    }
+
+}
