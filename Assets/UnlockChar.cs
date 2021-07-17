5bd833e0df4551b2e7dffcc87e2776731e0a6965
Tiago Gaspar <tiagongaspar@gmail.com>
2021-07-17 18:20:46 +0000

    hotfix

--- Assets/UnlockChar.cs
+++ Assets/UnlockChar.cs
@@ -11,19 +11,27 @@ public class UnlockChar : MonoBehaviour
         charclass = GameObject.Find("CharManager");
 
         //if unlocked turn off
-        if (charclass.GetComponent<CharClass>().charsUnlocked[charNumber - 1] == true)
-        {
-            gameObject.SetActive(false);
-        }
+        Unactivate(charNumber);
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            charclass.GetComponent<CharClass>().charsUnlocked[charNumber-1] = true;
+            charclass.GetComponent<CharClass>().charsUnlocked[charNumber - 1] = true;
             // become new sprite
             collision.GetComponent<PlayerSprite>().SetSprite(charNumber);
+            Unactivate(charNumber);
+            charclass.GetComponent<CharClass>().SaveChar(charNumber);
+        }
+    }
+
+    private void Unactivate(int charnumber)
+    {
+        if (charclass.GetComponent<CharClass>().charsUnlocked[charnumber - 1] == true)
+        {
+            gameObject.SetActive(false);
         }
     }
 }
