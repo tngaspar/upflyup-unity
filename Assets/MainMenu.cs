3d3bb28560b58d55f027cedc7b11dad153e89700
Tiago Gaspar <tiagongaspar@gmail.com>
2021-07-17 18:48:16 +0000

    reset confirmation menu implemented

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public GameObject emptyPlayer;
+    //public GameObject emptyPlayer;
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
@@ -18,8 +18,8 @@ public class MainMenu : MonoBehaviour
 
     public void ResetGame()
     {
-        //SaveSystem.DeleteFile(); //deprecated
-        emptyPlayer.GetComponent<Player>().ResetPlayer();
+        SaveSystem.DeleteFile(); //deprecated
+        //emptyPlayer.GetComponent<Player>().ResetPlayer();
     }
 
     public void DeleteGameSave()
