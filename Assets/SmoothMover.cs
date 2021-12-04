a45f3c8ed1e80e32c860ea6ad540e58bbd3d4d50
Tiago Gaspar <tiagongaspar@gmail.com>
2021-12-04 16:09:38 +0000

    added tutorial animation

---
+++ Assets/SmoothMover.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class SmoothMover : MonoBehaviour
+{
+    [SerializeField] private Transform start;
+    [SerializeField] private Transform end;
+    [SerializeField] private float loopDuration = 1f;
+    [SerializeField] private float deltaMultiplier = 1f;
+    private void Start()
+    {
+        transform.position = start.position;
+        InvokeRepeating("ObjectReset", loopDuration, loopDuration);
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.Lerp(transform.position, end.position, Time.deltaTime * deltaMultiplier);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawCube(start.position, Vector3.one * 0.1f);
+        Gizmos.DrawCube(end.position, Vector3.one * 0.1f);
+    }
+
+
+    private void ObjectReset()
+    {
+        transform.position = start.position;
+    }
+}
